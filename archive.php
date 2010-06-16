0572a23da4bb8a339c10e49531727e0841964083
Andrew Bruce <andrew@camelpunch.com>
2010-06-16 10:38:41 +0100

    more h1 / h2 fixing

--- archive.php
+++ archive.php
@@ -4,25 +4,25 @@
 		<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
  	  
  	  <?php /* If this is a category archive */ if (is_category()) { ?>
-		<h2 class="pagetitle">Category: <?php single_cat_title(); ?></h2>
+		<h1 class="pagetitle">Category: <?php single_cat_title(); ?></h1>
  	  
  	  <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-		<h2 class="pagetitle">Tagged: <?php single_tag_title(); ?></h2>
+		<h1 class="pagetitle">Tagged: <?php single_tag_title(); ?></h1>
  	  
  	  <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h2>
+		<h1 class="pagetitle">Archive for <?php the_time('F jS, Y'); ?></h1>
  	  
  	  <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h2>
+		<h1 class="pagetitle">Archive for <?php the_time('F, Y'); ?></h1>
  	  
  	  <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-		<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
+		<h1 class="pagetitle">Archive for <?php the_time('Y'); ?></h1>
 	  
 	  <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-		<h2 class="pagetitle">Author Archive</h2>
+		<h1 class="pagetitle">Author Archive</h1>
  	  
  	  <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-		<h2 class="pagetitle">Blog Archives</h2>
+		<h1 class="pagetitle">Blog Archives</h1>
  	 
  	  <?php } ?>
 		
